f7c203d6eac7f229bf25f587ecbfbee41ecc1eb0
ligross <ligros7@gmail.com>
2015-04-16 22:27:42 +0300

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('news.urls')),
+    url(r'^($|news/)', include('news.urls')),
     url(r'^articles/', include('articles.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
