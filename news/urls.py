3ee12184b1bc21efe32110504f6978f56f536b6c
ligross <ligros7@gmail.com>
2015-04-16 22:28:36 +0300

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^(?P<news_id>[0-9]+)/$', views.detail, name='detail')
 ]
