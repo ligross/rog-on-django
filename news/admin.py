f7d0bc592ec0912840e4cd61abc9f3807ce1a682
ligross <ligros7@gmail.com>
2015-04-21 18:11:55 +0300

    base html changes

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Image
 
 
 class NewsAdmin(admin.ModelAdmin):
--- news/models.py
+++ news/models.py
@@ -5,9 +5,8 @@ class News(models.Model):
     news_title = models.CharField(max_length=200)
     news_text = models.TextField()
     link_to_original = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='news', default=None, blank=True, null=True)
+    preview_image = models.ImageField(upload_to='news', default=None, blank=True, null=True)
     pub_date = models.DateTimeField('date published')
-
 
     def __str__(self):
         return self.news_title
