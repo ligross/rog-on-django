d776a5217c78547bd297c2e40a293a024cdbc84a
yulia_baranova <ligros7@gmail.com>
2015-05-10 22:33:32 +0300

    Adding resonsive tables plugin

--- news/suit-ckeditor/ckeditor/plugins/specialchar/dialogs/lang/fi.js
+++
@@ -1,1 +0,0 @@
-/usr/local/lib/python2.7/dist-packages/django_suit_ckeditor-0.0.2-py2.7.egg/suit_ckeditor/static/suit-ckeditor/ckeditor/plugins/specialchar/dialogs/lang/fi.js
--- news/suit/js/datepicker/locales/bootstrap-datepicker.sw.js
+++
@@ -1,1 +0,0 @@
-/usr/local/lib/python2.7/dist-packages/django_suit-0.3a1-py2.7.egg/suit/static/suit/js/datepicker/locales/bootstrap-datepicker.sw.js
