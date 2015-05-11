cc82f7e4f7aa6181b104fc39cda546f582f71b1b
yulia_baranova <ligros7@gmail.com>
2015-05-11 19:50:04 +0300

    Adding social-auth for vk

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.shortcuts import redirect
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -8,14 +9,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', include('news.urls')),
-    url(r'^$', include('news.urls')),
+    url(r'', include('social_auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^news/', include('news.urls')),
+    url(r'^social/', include('social_auth.urls')),
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^articles/', include('articles.urls')),
     url(r'^database/', include('database.urls')),
 )
